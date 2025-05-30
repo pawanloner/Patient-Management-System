34a62318554db7c51ca396b3f157d787a10eceea
pawanloner <pajitk20ecs@student.mes.ac.inm>
2025-05-30 23:10:30 +0530

    Initial commit

---
+++ src/main/java/patientService/patientServiceApplication.java
@@ -0,0 +1,13 @@
+package patientService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class patientServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(patientServiceApplication.class, args);
+	}
+
+}
